c25143f94f3bfc2e5a9aa79eaa35a9f5344d9bea
AndreJorgeLopes <chaoticgamerpt@gmail.com>
2021-03-31 11:10:14 +0100

    Added build for Netlify

---
+++ frontend/build/precache-manifest.334d0425dc3a3beff799d7f881c1587a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cee63bc8b50c4714e880fb677e41828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ddedd2eb806dc6af9ac",
+    "url": "/static/css/2.93675d08.chunk.css"
+  },
+  {
+    "revision": "23af41248f99071c75fd",
+    "url": "/static/css/main.e94e27cb.chunk.css"
+  },
+  {
+    "revision": "0ddedd2eb806dc6af9ac",
+    "url": "/static/js/2.61e76340.chunk.js"
+  },
+  {
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/static/js/2.61e76340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84dcd0557fc3fe0fd51",
+    "url": "/static/js/3.05bee00d.chunk.js"
+  },
+  {
+    "revision": "23af41248f99071c75fd",
+    "url": "/static/js/main.b79b0c2f.chunk.js"
+  },
+  {
+    "revision": "407f99f6e1d315a30eaf",
+    "url": "/static/js/runtime-main.237f1e9c.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "5c58d0e1d2ec48e766846af99eaeb6f8",
+    "url": "/static/media/flags.5c58d0e1.png"
+  },
+  {
+    "revision": "27670c535824abb6ae7263d60fdc8a86",
+    "url": "/static/media/flags@2x.27670c53.png"
+  },
+  {
+    "revision": "f68d4ad28843a8578a66c18fde00603c",
+    "url": "/static/media/logo-text.f68d4ad2.png"
+  },
+  {
+    "revision": "a154c2699b306f0ae677e0d9811268a9",
+    "url": "/static/media/logo.a154c269.png"
+  },
+  {
+    "revision": "fb923b82b1a1ee83c0b02fb9842e5dbe",
+    "url": "/static/media/logo_ext.fb923b82.png"
+  }
+]);
